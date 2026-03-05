6e50ca574f6fbe5da18f7b8345817f4e5fe19c56
Juan J Rebollo <jj.rebollo.barranco@gmail.com>
2026-03-05 23:25:28 +0100

    update prod base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Set this to your deployed backend URL before running `ng build`
   // e.g. https://your-backend.railway.app
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://api.3dprinter.jrebollo.dev'
 };
